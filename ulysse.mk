de24a30ac1d4c57a64b9c8348226f1969eb924f1
codeworkx <daniel.hillenbrand@codeworkx.de>
2019-05-06 20:57:12 +0530

    ulysse-common: build libbthost_if from hardware/qcom
    Change-Id: Ifbaf499a0825c288c642486db455c9350f45fe32

--- ulysse.mk
+++ ulysse.mk
@@ -175,6 +175,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor \
+    BluetoothQti \
+    libbthost_if \
     libbt-vendor
 
 PRODUCT_COPY_FILES += \
