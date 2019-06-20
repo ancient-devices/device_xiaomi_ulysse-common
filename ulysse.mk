e830dd23f5715c5add9834658c9612267b738356
Srinu Jella <sjella@codeaurora.org>
2019-06-20 18:21:19 +0530

    ulysse-common: Cleanup BT A2DP HAL entries from manifest
    - Removed HAL 1.0 entries from manifest as they
      are not supported.
    
    - Added Default A2DP HAL 2.0 entry to manifest.
    
    CRs-Fixed: 2472010
    Change-Id: Id8da896c99ab0904158715dcf9a74ccf73b77895

--- ulysse.mk
+++ ulysse.mk
@@ -160,6 +160,10 @@ PRODUCT_COPY_FILES += \
 	$(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
 	$(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth.audio@2.0
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
