e448c250adfa7d52701f69861e64749a8d35a414
Wang Han <416810799@qq.com>
2019-09-16 23:56:33 +0800

    ulysse-common: Import and bind mount loader config for media swcodec
     * This config needs to be modified for legacy devices. As
       there is no easy way to split it, import it into device
       tree and bind mount it instead.
    
     * Head at android-10.0.0_r2
       (3ac371484399da6efbd0c835aa461d3326263ab1).
    
    Change-Id: I8d5003d7cd3a471c569844aede4b5ea4c68ae36b

--- msm8953.mk
+++ msm8953.mk
@@ -69,6 +69,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# APEX
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/ld.config.txt:system/etc/swcodec/ld.config.txt
+
 # ANT
 PRODUCT_PACKAGES += \
     AntHalService \
