04b1c05692d8dbf23e3f153a95d7c82af519761b
GiaSen <giammasena@gmail.com>
2020-06-07 22:36:54 +0200

    UNOFFICIAL: ulysse-common: Update flame build fp to June release

--- init/init_ulysse.cpp
+++ init/init_ulysse.cpp
@@ -122,4 +122,7 @@ void vendor_load_properties()
     property_set("dalvik.vm.heaptargetutilization", heaptargetutilization);
     property_set("dalvik.vm.heapminfree", heapminfree);
     property_set("dalvik.vm.heapmaxfree", heapmaxfree);
+
+    // Pixel identification
+    property_override("ro.build.fingerprint","google/flame/flame:10/QQ3A.200605.001/6392402:user/release-keys");
 }
