81bf37fe59a1dd4c4480bf1f54bd63a5751d96d3
zqm64 <zqm64@outlook.com>
2020-08-13 16:26:12 +0800

    Revert "msm8953-common: Guard vndk-sp"
    This reverts commit 6afca9d2907a1c4cd06bb15b58cae53b57dcca3c.

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -1,5 +1,3 @@
-ifneq ($(BOARD_VNDK_VERSION),current)
-
 VNDK_SP_LIBRARIES := \
     android.hardware.graphics.allocator@2.0 \
     android.hardware.graphics.common@1.0 \
@@ -94,4 +92,3 @@ LOCAL_REQUIRED_MODULES := \
     $(addsuffix .vndk-ext-gen,$(EXTRA_VENDOR_LIBRARIES))
 include $(BUILD_PHONY_PACKAGE)
 
-endif
