4730a6fcd60878cfd9c90deb4afab3cdd24a306f
GiaSen <giammasena@gmail.com>
2020-09-11 16:16:35 +0200

    ulysse-common: copy vndk-29 libbinderthreadstate.so to vendor
    09-11 14:55:24.310  1517  1517 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/gx_fpd": library "libbinderthreadstate.so" not found: needed by /vendor/lib64/libbindergx.so in namespace (default)

--- ulysse.mk
+++ ulysse.mk
@@ -449,6 +449,9 @@ PRODUCT_PACKAGES += \
     vndk-ext \
     libstdc++.vendor
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libbinderthreadstate.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libbinderthreadstate.so
+
 # Thermal
 PRODUCT_PACKAGES += \
     thermal.msm8937
