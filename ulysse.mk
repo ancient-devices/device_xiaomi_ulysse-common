7171c3593fda825b5feb6b2be77d26e8b09965b5
bikram557 <kitkatmukherjee2015@gmail.com>
2020-09-19 15:46:17 +0000

    ulysse-common: build libhidltransport HIDL packages

--- ulysse.mk
+++ ulysse.mk
@@ -166,7 +166,9 @@ PRODUCT_COPY_FILES += \
 # Binder
 PRODUCT_PACKAGES += \
     libhwbinder \
-    libhwbinder.vendor
+    libhwbinder.vendor \
+    libhidltransport \
+    libhidltransport.vendor
 
 # Bluetooth
 PRODUCT_COPY_FILES += \
