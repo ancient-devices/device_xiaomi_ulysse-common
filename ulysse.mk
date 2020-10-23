142bfe419a2ba6f9d9be3d69b9181d2f7a8e2ae8
Scott Warner <Tortel1210@gmail.com>
2020-10-23 20:48:20 +0200

    ulysse-common: Use opensource btconfigstore
    Change-Id: Id16333c0b36fbf93e504fe6cb0107fce88721791

--- ulysse.mk
+++ ulysse.mk
@@ -171,6 +171,10 @@ PRODUCT_PACKAGES += \
     libhidltransport.vendor
 
 # Bluetooth
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
 
