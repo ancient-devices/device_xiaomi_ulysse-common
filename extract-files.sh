bb2429ccf3464cfb534315bf02af2696cc587c09
Michael Bestas <mkbestas@lineageos.org>
2020-12-28 01:17:48 +0200

    ulysse-common: Switch to standalone extract utils
    Align with templates while we are at it
    
    Change-Id: I3cd279191c7bd3582f1c1f80c588eb26cf9da933

--- extract-files.sh
+++ extract-files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2018-2020 The LineageOS Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -12,9 +12,9 @@ set -e
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-LINEAGE_ROOT="${MY_DIR}"/../../..
+ANDROID_ROOT="${MY_DIR}/../../.."
 
-HELPER="${LINEAGE_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -26,8 +26,8 @@ CLEAN_VENDOR=false
 
 ONLY_COMMON=
 ONLY_TARGET=
-SECTION=
 KANG=
+SECTION=
 
 while [ "${#}" -gt 0 ]; do
     case "${1}" in
@@ -60,34 +60,32 @@ fi
 
 if [ -z "${ONLY_TARGET}" ]; then
     # Initialize the helper for common device
-    setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${LINEAGE_ROOT}" true "${CLEAN_VENDOR}"
+    setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
 
-    extract "${MY_DIR}/proprietary-files-qc.txt" "${SRC}" \
-            "${KANG}" --section "${SECTION}"
+    extract "${MY_DIR}/proprietary-files-qc.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 fi
 
 if [ -z "${ONLY_COMMON}" ] && [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
     # Reinitialize the helper for device
     source "${MY_DIR}/../${DEVICE}/extract-files.sh"
-    setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" false "${CLEAN_VENDOR}"
+    setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
-    extract "${MY_DIR}/../${DEVICE}/proprietary-files.txt" "${SRC}" \
-            "${KANG}" --section "${SECTION}"
+    extract "${MY_DIR}/../${DEVICE}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 fi
 
 # Hax for cam configs
-sed -i "s|/system/etc/camera/|/vendor/etc/camera/|g" "$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary/vendor/lib/libmmcamera2_sensor_modules.so
+sed -i "s|/system/etc/camera/|/vendor/etc/camera/|g" "$ANDROID_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary/vendor/lib/libmmcamera2_sensor_modules.so
 
 # Wrap libgui_vendor into libwui
-sed -i "s/libgui/libwui/g" "$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary/vendor/lib/libmmsw_platform.so
-sed -i "s/libgui/libwui/g" "$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary/vendor/lib/libmmcamera_ppeiscore.so
-sed -i "s/libgui/libwui/g" "$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary/vendor/lib/libmmcamera2_stats_modules.so
-sed -i "s/libgui/libwui/g" "$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary/vendor/lib/libmmsw_detail_enhancement.so
-sed -i "s/libgui/libwui/g" "$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary/vendor/lib64/libmmsw_platform.so
-sed -i "s/libgui/libwui/g" "$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary/vendor/lib64/libmmsw_detail_enhancement.so
+sed -i "s/libgui/libwui/g" "$ANDROID_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary/vendor/lib/libmmsw_platform.so
+sed -i "s/libgui/libwui/g" "$ANDROID_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary/vendor/lib/libmmcamera_ppeiscore.so
+sed -i "s/libgui/libwui/g" "$ANDROID_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary/vendor/lib/libmmcamera2_stats_modules.so
+sed -i "s/libgui/libwui/g" "$ANDROID_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary/vendor/lib/libmmsw_detail_enhancement.so
+sed -i "s/libgui/libwui/g" "$ANDROID_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary/vendor/lib64/libmmsw_platform.so
+sed -i "s/libgui/libwui/g" "$ANDROID_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary/vendor/lib64/libmmsw_detail_enhancement.so
 
 # Shim libandroid for camera blobs
-patchelf --replace-needed libandroid.so libandroid_shim.so "$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary/vendor/lib/libmmcamera2_stats_modules.so
-patchelf --replace-needed libandroid.so libandroid_shim.so "$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary/vendor/lib/libmpbase.so
+patchelf --replace-needed libandroid.so libandroid_shim.so "$ANDROID_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary/vendor/lib/libmmcamera2_stats_modules.so
+patchelf --replace-needed libandroid.so libandroid_shim.so "$ANDROID_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary/vendor/lib/libmpbase.so
 
-"$MY_DIR"/setup-makefiles.sh
+"$MY_DIR/setup-makefiles.sh"
