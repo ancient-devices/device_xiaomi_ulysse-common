d9f2193e5113a1b3990c8bfe589b3d5bd83ea2a3
Reiss Historia <reisshxx@mail.gabuters-dev.my.id>
2021-03-15 12:20:18 +0700

    ulysse-common: Update redfin fp

--- init/init_ulysse.cpp
+++ init/init_ulysse.cpp
@@ -145,9 +145,9 @@ void vendor_load_properties()
     property_set("dalvik.vm.heapmaxfree", heapmaxfree);
 
     // Pixel identification
-    property_override("ro.build.description","redfin-user 11 RD1A.200810.020 6792687 release-keys");
-    property_override("ro.build.fingerprint","google/redfin/redfin:11/RD1A.200810.020/6792687:user/release-keys");
-    property_override("ro.system.build.fingerprint","google/redfin/redfin:11/RD1A.200810.020/6792687:user/release-keys");
+    property_override("ro.build.description","redfin-user 11 RQ2A.210305.006 7119741 release-keys");
+    property_override("ro.build.fingerprint","google/redfin/redfin:11/RQ2A.210305.006/7119741:user/release-keys");
+    property_override("ro.system.build.fingerprint","google/redfin/redfin:11/RQ2A.210305.006/7119741:user/release-key");
 
     // Front Flashlight
     property_set("persist.s5k3p8sp.flash.low","320");
